35f692f01275a6a68948cedfe1c6462c46fb0055
Vladimir Fituzov <dwanew@mail.ru>
2020-07-11 23:40:02 +0300

    first commit

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 import time
 
-start_time = datetime.now()
+start_time_1 = datetime.now()
 
-end_time = datetime.now()
+end_time_1 = datetime.now()
 exec_time = (end_time - start_time)
 print('Finished!')
 print('start_time: ', start_time)
-print('end_time: ', end_time)
+print('end_time_1: ', end_time_1)
 print('exec_time: ', exec_time) 
